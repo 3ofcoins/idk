dce27e0ec23ace67d7f2ecc088081bd60c9b7421
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-01 21:40:31 +0200

    Use git_describe version

--- config/projects/idk.rb
+++ config/projects/idk.rb
@@ -5,9 +5,7 @@ homepage "https://github.com/3ofcoins/idk/"
 replaces        "idk"
 install_path    "/opt/idk"
 
-version, wip = Omnibus::BuildVersion.new.semver.split('+')
-version = Gem::Version.new(version).bump.to_s << '.wip' << wip if wip
-build_version version
+build_version Omnibus::BuildVersion.new.git_describe
 build_iteration 1
 
 dependency "preparation"
