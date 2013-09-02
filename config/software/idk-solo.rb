90c45b78324285f9b04091edd46cfee481a53e70
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-02 17:06:38 +0200

    Fix berkshelf for idk-solo

--- config/software/idk-solo.rb
+++ config/software/idk-solo.rb
@@ -8,5 +8,9 @@ source :path => File.expand_path("files/#{name}", Omnibus.project_root)
 build do
   command "mkdir -p #{install_dir}/solo"
   command "#{install_dir}/embedded/bin/rsync --exclude='*~' --delete -a ./ #{install_dir}/solo"
-  command("#{install_dir}/embedded/bin/berks install --path cookbooks", :cwd => "#{install_dir}/solo", :env => Omnibus::Builder::BUNDLER_BUSTER.merge('OMNIBUS_ROOT' => Omnibus.project_root))
+  command "#{install_dir}/embedded/bin/berks install --path cookbooks",
+          :cwd => "#{install_dir}/solo",
+          :env => Omnibus::Builder::BUNDLER_BUSTER.merge(
+    'OMNIBUS_ROOT' => Omnibus.project_root,
+    'PATH' => "#{install_dir}/embedded/bin:#{ENV['PATH']}")
 end
