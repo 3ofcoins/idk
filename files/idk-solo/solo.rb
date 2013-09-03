769f723121233d8711a196a24f9453c90616a257
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-03 19:45:16 +0200

    Stuff.

--- files/idk-solo/solo.rb
+++ files/idk-solo/solo.rb
@@ -1,3 +1,4 @@
+_basedir = File.expand_path(File.dirname(__FILE__))
 file_cache_path "/var/local/idk/cache/chef-solo"
-cookbook_path "#{File.expand_path(File.dirname(__FILE__))}/cookbooks"
+cookbook_path [ "#{_basedir}/site-cookbooks", "#{_basedir}/cookbooks" ]
 verbose_logging false
