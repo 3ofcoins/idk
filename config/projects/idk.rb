8d527ca05874ab0421bb53ab08eaa51dd2b3b75b
Maciej Pasternacki <maciej@3ofcoins.net>
2014-01-14 16:01:58 +0100

    Go semver.

--- config/projects/idk.rb
+++ config/projects/idk.rb
@@ -5,7 +5,7 @@ homepage "https://github.com/3ofcoins/idk/"
 replaces        "idk"
 install_path    "/opt/idk"
 
-build_version Omnibus::BuildVersion.new.git_describe
+build_version Omnibus::BuildVersion.new.semver
 build_iteration 1
 
 dependency "preparation"
