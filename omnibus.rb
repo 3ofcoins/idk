963a063a10bf9c2852890103200a8c0519ed3f77
Maciej Pasternacki <maciej@3ofcoins.net>
2014-04-14 21:50:13 +0200

    Whielist libicucore

--- omnibus.rb
+++ omnibus.rb
@@ -1,3 +1,4 @@
 # https://github.com/opscode/omnibus-ruby/commit/c01f85515bb3028609c22f99813e66562fb29b84
 # not yet released
 Omnibus::HealthCheck::MAC_WHITELIST_LIBS << /CoreServices/
+Omnibus::HealthCheck::MAC_WHITELIST_LIBS << /libicucore/
